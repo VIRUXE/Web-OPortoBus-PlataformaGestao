b9ca77b611af04feb9cdbb1c11ad6b86bd09f908
VIRUXE <viruxe@users.noreply.github.com>
2020-02-28 14:55:51 +0000

    Icon de Utilizador
    Adicionados Icons de Utilizador na Página de Abastecimentos

--- includes/utilizador.class.php
+++ includes/utilizador.class.php
@@ -1,16 +1,9 @@
 <?php
-/**
- * 
- */
+
 class Utilizador
 {
-	
-	/*function __construct(argument)
-	{
-		# code...
-	}*/
 
-	function FormatarNome()
+	static function FormatarNome()
 	{
 		$nome = NULL;
 
@@ -19,6 +12,34 @@ class Utilizador
 
 		return $nome;
 	}
-}
 
-?>
+	// 'OUTRO','VIGILANTE','MOTORISTA','MOTORISTAPESADOS','DONO','DESENVOLVEDOR'
+
+	static function Icon($userTelemovel)
+	{
+		global $database;
+	 
+		$icon = "fas fa-user-alien";
+
+		$result = $database->query("SELECT cargo FROM utilizadores WHERE telemovel = '$userTelemovel' LIMIT 1");
+		if($result && $result->num_rows > 0)
+		{
+			switch ($result->fetch_assoc()['cargo'])
+			{
+				case 'DESENVOLVEDOR':
+					$icon = "fas fa-user-secret";
+					break;
+
+				case 'DONO':
+					$icon = "fas fa-user-crown";
+					break;
+
+				case 'MOTORISTAPESADOS':
+					$icon = "fas fa-user-tie";
+					break;
+			}
+		}
+
+		return $icon;
+	}
+}
