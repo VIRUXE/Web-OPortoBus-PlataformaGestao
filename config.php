ca9492e691672ce38149d343b103cf649f262a4b
VIRUXE <viruxe@users.noreply.github.com>
2020-03-01 20:49:23 +0000

    Configurações para o Mail

--- config.php
+++ config.php
@@ -3,4 +3,8 @@ define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', null);
 define('DB_NAME', 'oportobus_plataforma');
+
+ini_set('SMTP', "smtp.oportobus.pt");
+ini_set('smtp_port', "25");
+ini_set('sendmail_from', "no-reply@oportobus.pt");
 ?>
