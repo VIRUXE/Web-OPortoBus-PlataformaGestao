f35410b843dc533c8a4fc6e8e71cd122fe2ceafb
VIRUXE <viruxe@users.noreply.github.com>
2020-03-03 19:01:16 +0000

    Sessões de Condução
    Ir buscar a sessão ativa ao voltar a fazer login e outras pequenas merdas

--- includes/views/frota/conducao.php
+++ includes/views/frota/conducao.php
@@ -2,34 +2,68 @@
 require 'includes/frota/frota.class.php';
 
 // Carregar a sessão activa se existir
+$result = $database->query("SELECT viatura_matricula, kms_iniciais, obs FROM viaturas_sessoes WHERE funcionario_telemovel = '{$_SESSION['user']->telemovel}' AND ativa = true");
+
+if (!$result)
+	trigger_error('Query Inválida: ' . $database->error);
+else
+{
+	if ($result->num_rows > 0)
+	{
+		$conducao = $result->fetch_assoc();
+
+		$_SESSION['user']->conducao = 
+		[
+			'viatura' 		=> $conducao['viatura_matricula'], 
+			'kms' 			=> $conducao['kms_iniciais'], 
+			'gps' 			=> NULL, 
+			'observacoes' 	=> $conducao['obs']
+		];
+	}
+}
+
+
 
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	// Verificar se está a abrir ou fechar condução
-	if(isset($_SESSION['utilizador']['conducao'])) // Fechar Condução
+	if(!empty($_SESSION['user']->conducao)) // Fechar Condução
 	{
-		$sessao = $_SESSION['utilizador']['conducao']; // Só para ser mais fácil...
+
+		$coords = explode(',',$_POST['localizacao']); // Separar por latitude e longitude
+
+		$_SESSION['user']->conducao['kms'] = $_POST['viaturaKms'];
+		$_SESSION['user']->conducao['gps'] = ['latitude' => $coords[0], 'longitude' => $coords[1]];
+		$_SESSION['user']->conducao['observacoes'] = $_POST['conducaoObservacoes'];
+
+		$sessao = $_SESSION['user']->conducao; // Só para ser mais fácil...
+
+		var_dump($sessao);
 
 		$result = $database->query("
 			UPDATE viaturas_sessoes 
-			SET data_final = current_timestamp(), kms_finais = '{$sessao['kms']}', localizacao_final = '".json_encode($sessao['gps'])."', obs = '{$sessao['observacoes']}', activa = false
-			WHERE funcionario_telemovel = '{$_SESSION['utilizador']['telemovel']}' AND activa = true"
+			SET data_final = current_timestamp(), kms_finais = '{$sessao['kms']}', localizacao_final = '".json_encode($sessao['gps'])."', obs = '{$sessao['observacoes']}', ativa = false
+			WHERE funcionario_telemovel = '{$_SESSION['user']->telemovel}' AND ativa = true"
 		);
 
 		if (!$result)
 			trigger_error('Query Inválida: ' . $database->error);
 		else
-			echo '<div class="alert alert-danger text-center"><i class="fas fa-road"></i> Sessão de condução terminada para a viatura <span class="font-weight-bold">' . Viatura::FormatarMatricula($sessao['viatura']) . '</span></div>';
+			echo '
+				<div class="alert alert-danger text-center">
+				<i class="fas fa-road"></i> Sessão de condução terminada para a viatura <span class="font-weight-bold">' . Viatura::FormatarMatricula($sessao['viatura']) . '</span>
+				</div>
+			';
 
 		// Destruir a Sessão de Condução
-		unset($_SESSION['utilizador']['conducao']);
+		$_SESSION['user']->conducao = [];
 	}
 	else // Abrir Condução
 	{
 		$coords = explode(',',$_POST['localizacao']); // Separar por latitude e longitude
 
 		// Construir o array com os dados de condução na SESSION
-		$_SESSION['utilizador']['conducao'] = 
+		$_SESSION['user']->conducao = 
 		[
 			'viatura' 		=> $_POST['conducaoViatura'], 
 			'kms' 			=> $_POST['viaturaKms'], 
@@ -37,11 +71,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 			'observacoes' 	=> $_POST['conducaoObservacoes']
 		];
 
-		$sessao = $_SESSION['utilizador']['conducao']; // Só para ser mais fácil...
+		$sessao = $_SESSION['user']->conducao; // Só para ser mais fácil...
 
 		$result = $database->query("
 			INSERT INTO viaturas_sessoes (viatura_matricula, funcionario_telemovel, kms_iniciais, localizacao_inicial, obs) 
-			VALUES('{$sessao['viatura']}', '{$_SESSION['utilizador']['telemovel']}', '{$sessao['kms']}', '".json_encode($sessao['gps'])."', '{$sessao['observacoes']}')"
+			VALUES('{$sessao['viatura']}', '{$_SESSION['user']->telemovel}', '{$sessao['kms']}', '".json_encode($sessao['gps'])."', '{$sessao['observacoes']}')"
 		);
 
 		if (!$result)
@@ -54,13 +88,13 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	}
 }
 
-$sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
+$sessaoAtiva = isset($_SESSION['user']->conducao['viatura']) ? true : false;
 ?>
 			<h1 class="h3 mb-2 text-gray-800">Sessões de Condução</h1>
 
 			<div class="card shadow mb-4">
 				<div class="card-header py-3">
-					<h6 class="m-0 font-weight-bold text-<?= $sessaoAtiva ? "danger":"success" ?>"><?= $sessaoAtiva ? "Fecho de Condução - Viatura ".Viatura::FormatarMatricula($_SESSION['utilizador']['conducao']['viatura']) : "Início de Condução" ?></h6>
+					<h6 class="m-0 font-weight-bold text-<?= $sessaoAtiva ? "danger":"success" ?>"><?= $sessaoAtiva ? "Fecho de Condução - Viatura ".Viatura::FormatarMatricula($_SESSION['user']->conducao['viatura']) : "Início de Condução" ?></h6>
 				</div>
 				<div class="card-body">
 					<form class="form-horizontal" action="index.php?ver=frota&categoria=conducao" method="POST">
@@ -84,11 +118,11 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 						</div>
 						<div class="form-group row">
 							<label for="viaturaKms" class="col-md-2">KMs de <?= $sessaoAtiva ? "Fecho":"Abertura"?></label>					
-							<input type="number" id="viaturaKms" name="viaturaKms" class="col-md-10 form-control form-control-sm"<?= $sessaoAtiva ? ' value="'.$_SESSION['utilizador']['conducao']['kms'].'"' : NULL ?>required>
+							<input type="number" id="viaturaKms" name="viaturaKms" class="col-md-10 form-control form-control-sm"<?= $sessaoAtiva ? ' value="'.$_SESSION['user']->conducao['kms'].'"' : NULL ?>required>
 						</div>
 						<div class="form-group row">
 							<label for="conducaoObservações" class="col-md-2">Observações</label>					
-							<textarea id="conducaoObservacoes" name="conducaoObservacoes" class="col-md-10 form-control form-control-sm" rows="4" maxlength="255" placeholder="Qualquer tipo de observação sobre a viatura ou o serviço em si..."><?= $sessaoAtiva ? $_SESSION['utilizador']['conducao']['observacoes'] : NULL ?></textarea>
+							<textarea id="conducaoObservacoes" name="conducaoObservacoes" class="col-md-10 form-control form-control-sm" rows="4" maxlength="255" placeholder="Qualquer tipo de observação sobre a viatura ou o serviço em si..."><?= $sessaoAtiva ? $_SESSION['user']->conducao['observacoes'] : NULL ?></textarea>
 						</div>
 						<button type="submit" name="formConducao" class="btn btn-<?= $sessaoAtiva ? "danger":"success" ?> btn-icon-split my-1">
 							<span class="icon text-white-50">
@@ -99,6 +133,7 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 					</form>
 				</div>
 			</div>
+<?php if($_SESSION['user']->cargo == 'DONO' || $_SESSION['user']->cargo == 'DESENVOLVEDOR') { ?>			
 			<div class="card shadow mb-4">
 	<div class="card-header py-3">
 		<h6 class="m-0 font-weight-bold text-primary">Listagem de Sessões de Condução</h6>
@@ -108,7 +143,6 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 			<table class="table table-sm table-borderless table-hover" id="dataTable" width="100%" cellspacing="0">
 				<thead>
 					<tr>
-						<th class="text-left">ID</th>
 						<th class="text-center">Viatura</th>
 						<th class="text-center">Funcionário</th>
 						<th class="text-center">Data Inicial</th>
@@ -121,7 +155,6 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 				</thead>
 				<tfoot>
 					<tr>
-						<th class="text-left">ID</th>
 						<th class="text-center">Viatura</th>
 						<th class="text-center">Funcionário</th>
 						<th class="text-center">Data Inicial</th>
@@ -135,9 +168,11 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 				<tbody>
 					<?php
 					$result = $database->query("
-									SELECT id, viatura_matricula, funcionario_telemovel, CONCAT(u.nome_primeiro, '. ',SUBSTRING(u.nome_ultimo,1,1)) as motorista, data_inicial, kms_iniciais, localizacao_inicial, data_final, kms_finais, localizacao_final, obs, activa FROM viaturas_sessoes 
-									LEFT JOIN utilizadores u ON viaturas_sessoes.funcionario_telemovel = u.telemovel 
-									ORDER BY activa DESC
+									SELECT id, viatura_matricula, v.tipo as viatura_tipo, funcionario_telemovel, CONCAT(u.nome_primeiro, '. ',SUBSTRING(u.nome_ultimo,1,1)) as motorista, data_inicial, kms_iniciais, localizacao_inicial, data_final, kms_finais, localizacao_final, obs, ativa 
+									FROM viaturas_sessoes 
+									LEFT JOIN utilizadores u ON viaturas_sessoes.funcionario_telemovel = u.telemovel
+									LEFT JOIN viaturas v ON viaturas_sessoes.viatura_matricula = v.matricula 
+									ORDER BY ativa DESC, data_inicial DESC
 								");
 
 					if (!$result)
@@ -146,9 +181,8 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 					{
 						while ($conducao = $result->fetch_assoc()) 
 						{
-							echo '<tr'.($conducao['activa'] ? ' class="table-success"' : NULL).'" title="'.($conducao['obs'] ? $conducao['obs'] : "Sem observações...").'">';
-							echo '<th class="text-left">' . $conducao['id'] . '</th>';
-							echo '<td class="text-center">' . Viatura::FormatarMatricula($conducao["viatura_matricula"]) . '</a></td>';
+							echo '<tr'.($conducao['ativa'] ? ' class="table-success"' : NULL).' title="Observações: '.($conducao['obs'] ? $conducao['obs'] : "Sem observações...").'">';
+							echo '<td class="text-center">' . '<i class="' . Viatura::Icon($conducao["viatura_tipo"]) . '"></i> ' . Viatura::FormatarMatricula($conducao["viatura_matricula"]) . '</td>';
 							echo '<td class="text-center"><i class="' . Utilizador::Icon($conducao["funcionario_telemovel"]) . '"></i> ' . $conducao['motorista'] . '</td>';
 							echo '<td class="text-center">' . $conducao['data_inicial'] . '</td>';
 							echo '<td class="text-center">' . $conducao['kms_iniciais'] . '</td>';
@@ -156,7 +190,6 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 							echo '<td class="text-center">' . $conducao['data_final'] . '</td>';
 							echo '<td class="text-right">' . $conducao['kms_finais'] . '</td>';
 							// echo '<td class="text-right">' . $conducao['localizacao_final'] . '</td>';
-							// echo '<td class="text-right">' .  . '</td>';
 							echo '</tr>';
 						}
 					}
@@ -166,3 +199,4 @@ $sessaoAtiva = isset($_SESSION['utilizador']['conducao']) ? true : false;
 		</div>
 	</div>
 </div>
+<?php } ?>
