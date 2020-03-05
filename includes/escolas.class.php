f023d2cb15d7aab9cfca22ae61181c2fb4532da3
VIRUXE <viruxe@users.noreply.github.com>
2020-03-05 01:24:55 +0000

    Página para Inserir Crianças

--- includes/escolas.class.php
+++ includes/escolas.class.php
@@ -1,8 +1,4 @@
 <?php
-
-/**
- * 
- */
 class Escolas
 {
 	public static function Obter()
@@ -11,7 +7,7 @@ class Escolas
 
 		$escolas = [];
 
-		$result = $database->query("SELECT id, nome FROM localizacoes WHERE tipo = 0 ORDER BY nome ASC");
+		$result = $database->query("SELECT id, nome FROM localizacoes WHERE tipo = 1 ORDER BY nome ASC");
 		if($result && $result->num_rows)
 		{
 			while ($escola = $result->fetch_assoc())
--- includes/views/transporteescolar/criancas.php
+++ includes/views/transporteescolar/criancas.php
@@ -1,7 +1,19 @@
 <?php
 require_once 'includes/escolas.class.php';
 
+if($_SERVER["REQUEST_METHOD"] == "POST")
+{
+	$primeiroNome 	= $_POST['primeiroNome'];
+	$ultimoNome 	= (!empty($_POST['ultimoNome']) ? $_POST['ultimoNome'] : NULL);
+	$escolaID 		= $_POST['escola'];
+	$obs 			= $_POST['obs'];
 
+	if($database->query("INSERT INTO escolas_criancas (nome_primeiro, nome_ultimo, escola_id, obs) VALUES ('$primeiroNome', '$ultimoNome', '$escolaID', '$obs')"))
+	    echo "Inserido com sucesso";
+	else
+	    echo "Erro: ".$database->error;
+
+}
 ?>
 <div class="card o-hidden border-0 shadow-lg my-5">
 	<div class="card-body p-0">
@@ -11,18 +23,18 @@ require_once 'includes/escolas.class.php';
 					<div class="text-center">
 						<h1 class="h4 text-gray-900 mb-4">Inserir Criança</h1>
 					</div>
-					<form class="user">
+					<form method="POST">
 						<div class="form-group row">
 							<div class="col-sm-6 mb-3 mb-sm-0">
-								<input type="text" class="form-control" id="exampleFirstName" placeholder="Primeiro Nome" required>
+								<input type="text" class="form-control" name="primeiroNome" placeholder="Primeiro Nome" required>
 							</div>
 							<div class="col-sm-6">
-								<input type="text" class="form-control" id="exampleLastName" placeholder="Último Nome" required>
+								<input type="text" class="form-control" name="ultimoNome" placeholder="Último Nome">
 							</div>
 						</div>
 						<div class="form-group">
 							<legend>Escola</legend>
-							<select class="form-control" required>
+							<select class="form-control" name="escola" required>
 								<option value="" selected>Escolher...</option>
 								<?php
 								foreach(Escolas::Obter() as $escola)
@@ -32,12 +44,16 @@ require_once 'includes/escolas.class.php';
 						</div>
 						<div class="form-group">
 							<legend>Observações</legend>
-							<textarea class="form-control" placeholder="Qualquer observação sobre a criança..."></textarea>
+							<textarea class="form-control" name="obs" placeholder="Qualquer observação sobre a criança..."></textarea>
 						</div>
-						<button class="btn btn-primary">Registar Criança</button>
+						<button class="btn btn-success">Inserir Criança</button>
 					</form>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
+
+<!-- SELECT e.id, nome_primeiro, nome_ultimo, loc.nome AS escola 
+FROM escolas_criancas e
+LEFT JOIN localizacoes loc ON e.escola_id = loc.id -->
