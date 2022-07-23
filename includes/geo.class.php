c827959bc38071b9182698f0bdb1c2484eb5c0b8
VIRUXE <flavioaspereira@gmail.com>
2022-07-23 22:14:34 +0100

    fix geolocation handling
    fail safe for when we're not using google maps api

--- includes/geo.class.php
+++ includes/geo.class.php
@@ -1,15 +1,18 @@
 <?php
 class GEO
 {
-	public static function ObterEnderecoPorCoords($latitude, $longitude)
+
+	public static function ObterEnderecoPorCoords($location)
 	{
-		$geocode=file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$latitude.','.$longitude.'&sensor=false&key='.GOOGLEMAPS_KEY);
-		$output= json_decode($geocode);
+		if(!defined("GOOGLEMAPS_KEY") || is_null(GOOGLEMAPS_KEY)) return "Desconhecido";
+
+		$location = json_decode($location);	// location param gets passed as json so we need to decode it
+		$geocode = file_get_contents('https://maps.googleapis.com/maps/api/geocode/json?latlng='.$location["latitude"].','.$location["longitude"].'&sensor=false&key='.GOOGLEMAPS_KEY);
+		$output  = json_decode($geocode);
+
 		// echo '<pre>' , var_dump($output) , '</pre>';
 
-		$formattedAddress = ["rua" => @$output->results[0]->address_components[1]->short_name, "cidade" => @$output->results[0]->address_components[2]->short_name];
-
-		return $formattedAddress;
+		return sprintf("%s, %s", $output->results[0]->address_components[1]->short_name, $output->results[0]->address_components[2]->short_name);
 	}
 }
 ?>
--- includes/views/empresa/frota/conducao.php
+++ includes/views/empresa/frota/conducao.php
@@ -11,14 +11,14 @@ else
 {
 	if ($result->num_rows > 0)
 	{
-		$conducao = $result->fetch_assoc();
+		$session = $result->fetch_assoc();
 
-		$_SESSION['user']->conducao = 
+		$_SESSION['user']->session = 
 		[
-			'viatura' 		=> $conducao['viatura_matricula'], 
-			'kms' 			=> $conducao['kms_iniciais'], 
+			'viatura' 		=> $session['viatura_matricula'], 
+			'kms' 			=> $session['kms_iniciais'], 
 			'gps' 			=> NULL, 
-			'observacoes' 	=> $conducao['obs']
+			'observacoes' 	=> $session['obs']
 		];
 	}
 }
@@ -26,34 +26,34 @@ else
 if ($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	// Verificar se está a abrir ou fechar condução
-	if(!empty($_SESSION['user']->conducao)) // Fechar Condução
+	if(!empty($_SESSION['user']->session)) // Fechar Condução
 	{
-		if($_POST['viaturaKms'] > $_SESSION['user']->conducao['kms'])
+		if($_POST['viaturaKms'] > $_SESSION['user']->session['kms'])
 		{
 			if(strpos($_POST['localizacao'], ",")) // Verificar se foi fornecida posição GPS
 			{
 				$coords = explode(',',$_POST['localizacao']);
 				$coords = json_encode(['latitude' => $coords[0], 'longitude' => $coords[1]]); // Criar um JSON com os dados GPS
 
-				$_SESSION['user']->conducao['kms'] 			= $_POST['viaturaKms'];
-				$_SESSION['user']->conducao['gps'] 			= $coords;
-				$_SESSION['user']->conducao['observacoes'] 	= $_POST['conducaoObservacoes'];
+				$_SESSION['user']->session['kms'] 			= $_POST['viaturaKms'];
+				$_SESSION['user']->session['gps'] 			= $coords;
+				$_SESSION['user']->session['observacoes'] 	= $_POST['conducaoObservacoes'];
 
-				$conducao = $_SESSION['user']->conducao; // Só para ser mais fácil...
+				$session = $_SESSION['user']->session; // Só para ser mais fácil...
 
 				$result = $database->query("
 					UPDATE viaturas_sessoes 
-					SET data_final = current_timestamp(), kms_finais = '{$conducao['kms']}', localizacao_final = NULLIF('{$conducao['gps']}', ''), obs = '{$conducao['observacoes']}', ativa = false
+					SET data_final = current_timestamp(), kms_finais = '{$session['kms']}', localizacao_final = NULLIF('{$session['gps']}', ''), obs = '{$session['observacoes']}', ativa = false
 					WHERE funcionario_telemovel = '{$_SESSION['user']->telemovel}' AND ativa = true"
 				);
 
 				if (!$result)
 					trigger_error('Query Inválida: ' . $database->error);
 				else
-					Alerta('Sessão de condução <span class="font-weight-bold">terminada</span> para a viatura <span class="font-weight-bold">' . Viatura::FormatarMatricula($conducao['viatura']) . '</span>');
+					Alerta('Sessão de condução <span class="font-weight-bold">terminada</span> para a viatura <span class="font-weight-bold">' . Viatura::FormatarMatricula($session['viatura']) . '</span>');
 
 				// Destruir a Sessão de Condução
-				$_SESSION['user']->conducao = [];
+				$_SESSION['user']->session = [];
 			}
 			else
 				Alerta('Tem de ativar o GPS no seu dispositivo antes de poder <span class="font-weight-bold">fechar</span> a sessão!', ALERTA_ERRO, "map-marker-alt");
@@ -69,7 +69,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 			$coords = json_encode(['latitude' => $coords[0], 'longitude' => $coords[1]]); // Criar um JSON com os dados GPS
 
 			// Construir o array com os dados de condução na SESSION
-			$_SESSION['user']->conducao = 
+			$_SESSION['user']->session = 
 			[
 				'viatura' 		=> $_POST['conducaoViatura'], 
 				'tipo' 			=> $_POST['tipoConducao'], 
@@ -78,17 +78,17 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 				'observacoes' 	=> $_POST['conducaoObservacoes']
 			];
 
-			$conducao = $_SESSION['user']->conducao; // Só para ser mais fácil...
+			$session = $_SESSION['user']->session; // Só para ser mais fácil...
 
 			$result = $database->query("
 				INSERT INTO viaturas_sessoes (viatura_matricula, tipo, funcionario_telemovel, kms_iniciais, localizacao_inicial, obs) 
-				VALUES('{$conducao['viatura']}', '{$conducao['tipo']}', '{$_SESSION['user']->telemovel}', '{$conducao['kms']}', NULLIF('{$conducao['gps']}', ''), '{$conducao['observacoes']}')
+				VALUES('{$session['viatura']}', '{$session['tipo']}', '{$_SESSION['user']->telemovel}', '{$session['kms']}', NULLIF('{$session['gps']}', ''), '{$session['observacoes']}')
 			");
 
 			if (!$result)
 				trigger_error('Query Inválida: ' . $database->error);
 			else
-				Alerta('Sessão de condução <span class="font-weight-bold">iniciada</span> para a viatura <span class="font-weight-bold">' . Viatura::FormatarMatricula($conducao['viatura']) . '</span>', ALERTA_SUCESSO, "road");
+				Alerta('Sessão de condução <span class="font-weight-bold">iniciada</span> para a viatura <span class="font-weight-bold">' . Viatura::FormatarMatricula($session['viatura']) . '</span>', ALERTA_SUCESSO, "road");
 		}
 		else
 			Alerta('
@@ -99,16 +99,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 	}
 }
 
-$sessaoAtiva = isset($_SESSION['user']->conducao['viatura']) ? true : false;
+$sessaoAtiva = isset($_SESSION['user']->session['viatura']) ? true : false;
 ?>
 <h1 class="h3 mb-2 text-gray-800">Sessões de Condução</h1>
 <div class="alert alert-warning"><span class="font-weight-bolder">Atenção:</span> As sessões são para '<i>Iniciar</i>' e '<i>Fechar</i>' <u>sempre</u> que se começa ou deixa de <u>conduzir</u>.</div>
 <div class="card shadow mb-4">
 	<div class="card-header py-3">
-		<h6 class="m-0 font-weight-bold text-<?= $sessaoAtiva ? "danger":"success" ?>"><?= $sessaoAtiva ? 'Fecho de Condução - <span class="font-weight-bolder">Viatura '.Viatura::FormatarMatricula($_SESSION['user']->conducao['viatura']).'</span>' : 'Início de Condução' ?></h6>
+		<h6 class="m-0 font-weight-bold text-<?= $sessaoAtiva ? "danger":"success" ?>"><?= $sessaoAtiva ? 'Fecho de Condução - <span class="font-weight-bolder">Viatura '.Viatura::FormatarMatricula($_SESSION['user']->session['viatura']).'</span>' : 'Início de Condução' ?></h6>
 	</div>
 	<div class="card-body">
-		<form action="index.php?ver=empresa&categoria=frota&subcategoria=conducao" method="POST">
+		<form action="index.php?ver=empresa&categoria=frota&subcategoria=session" method="POST">
 			<?php if(!$sessaoAtiva)	{ ?>
 			<div class="form-row">
 				<div class="form-group col-md-auto">
@@ -134,11 +134,11 @@ $sessaoAtiva = isset($_SESSION['user']->conducao['viatura']) ? true : false;
 			<?php } ?>
 			<div class="form-group">
 				<label for="viaturaKms">KMs <?= $sessaoAtiva ? "Finais":"Iníciais"?></label>					
-				<input type="number" id="viaturaKms" name="viaturaKms" class="form-control form-control-sm"<?= $sessaoAtiva ? ' value="'.$_SESSION['user']->conducao['kms'].'"' : NULL ?>required>
+				<input type="number" id="viaturaKms" name="viaturaKms" class="form-control form-control-sm"<?= $sessaoAtiva ? ' value="'.$_SESSION['user']->session['kms'].'"' : NULL ?>required>
 			</div>
 			<div class="form-group">
 				<label for="conducaoObservações">Observações</label>					
-				<textarea id="conducaoObservacoes" name="conducaoObservacoes" class="form-control form-control-sm" rows="4" maxlength="255" placeholder="Qualquer tipo de observação sobre a viatura ou o serviço em si..."><?= $sessaoAtiva ? $_SESSION['user']->conducao['observacoes'] : NULL ?></textarea>
+				<textarea id="conducaoObservacoes" name="conducaoObservacoes" class="form-control form-control-sm" rows="4" maxlength="255" placeholder="Qualquer tipo de observação sobre a viatura ou o serviço em si..."><?= $sessaoAtiva ? $_SESSION['user']->session['observacoes'] : NULL ?></textarea>
 			</div>
 			<div class="form-group">
 				<label for="localizacao">Localização</label>					
@@ -182,6 +182,11 @@ $sessaoAtiva = isset($_SESSION['user']->conducao['viatura']) ? true : false;
 				</tfoot>
 				<tbody>
 					<?php
+					function FormatLocationURL($location) {
+						return sprintf('https://www.google.com/maps/search/%s,%s/', $location["latitude"], $location['longitude']);
+					}
+
+					// This function will only be used in this page. No need to place it anywhere else
 					function ServicoIcon($servico)
 					{
 						$icon = "fas fa-question-circle";
@@ -218,27 +223,38 @@ $sessaoAtiva = isset($_SESSION['user']->conducao['viatura']) ? true : false;
 					{
 						$dataHoje = date("d-m");
 
-						while ($conducao = $result->fetch_assoc()) 
+						while ($session = $result->fetch_assoc()) 
 						{
 							$foiHoje = false;
+							$location = [];
+							$address = [];
 
-							if(date('d-m', strtotime($conducao['data_inicial'])) == $dataHoje)
+							if(date('d-m', strtotime($session['data_inicial'])) == $dataHoje)
 								$foiHoje = true;
 
-							$locInicial 		= (!is_null($conducao['localizacao_inicial']) 	? json_decode($conducao['localizacao_inicial'], true) : 							NULL);
-							$enderecoInicial 	= (!is_null($locInicial) 						? GEO::ObterEnderecoPorCoords($locInicial['latitude'], $locInicial['longitude']) : 	NULL);
-							$locFinal 			= (!is_null($conducao['localizacao_final']) 	? json_decode($conducao['localizacao_final'], true) : 								NULL);
-							$enderecoFinal		= (!is_null($locFinal) 							? GEO::ObterEnderecoPorCoords($locFinal['latitude'], $locFinal['longitude']) : 		NULL);
+							if(!is_null($session['localizacao_inicial']) && !is_null($session['localizacao_final'])) {
+								$location["start"] 	= json_decode($session['localizacao_inicial'], true);
+								$location["finish"] = json_decode($session['localizacao_final'], true);
+							}
+							
+							// Get addresses from coordinatess
 
-							$kmsPercorridos 	= $conducao['kms_finais']-$conducao['kms_iniciais'];
+							$kmsPercorridos = $session['kms_finais']-$session['kms_iniciais'];
 
-							echo '<tr'.($conducao['ativa'] ? ($foiHoje ? ' class="table-success font-weight-bold"' : ' class="table-success"') : ($foiHoje ? ' class="font-weight-bold"' : NULL)).'>';
-							echo '<td nowrap><i style="color: Tomato;" class="fa'.($conducao['obs'] ? 's' : 'l').' fa-exclamation-circle" title="Observações:" data-toggle="popover" data-placement="top" data-content="'.($conducao['obs'] ? $conducao['obs'] : "Sem observações...").'"></i> <i class="'.ServicoIcon($conducao['tipo']).'"></i></td>';
-							echo '<td class="text-left" nowrap>'.date('d-m', strtotime($conducao['data_inicial'])).'</td>';
-							echo '<td class="text-center" nowrap>'.'<i class="'.Viatura::Icon($conducao["viatura_tipo"]).'"></i> '.Viatura::FormatarMatricula($conducao["viatura_matricula"]).'</td>';
-							echo '<td class="text-center" nowrap><a href="'.(!is_null($enderecoInicial) ? 'https://www.google.com/maps/search/'.$locInicial['latitude'].','.$locInicial['longitude'].'/' : '#').'" target="_blank">'.date('H:i', strtotime($conducao['data_inicial'])).'<br/><small class="text-xs">('.(!is_null($enderecoInicial) ? $enderecoInicial['rua'].', '.$enderecoInicial['cidade'] : 'Desconhecido').')</small></a></td>';
-							echo '<td class="text-center" nowrap>'.(!is_null($enderecoFinal) ? '<a href="https://www.google.com/maps/search/'.$locFinal['latitude'].','.$locFinal['longitude'].'/" target="_blank">'.date('H:i', strtotime($conducao['data_final'])).'<br/><small class="text-xs">('.$enderecoFinal['rua'].', '.$enderecoFinal['cidade'].')</small></a>' : NULL).'</td>';
-							echo '<td class="text-right" title="Quilometros" data-toggle="popover" data-placement="top" data-content="Iniciais: '.$conducao['kms_iniciais'].' Finais: '.($kmsPercorridos > 0 ? $conducao['kms_finais'] : "Desconhecido").'">'.($kmsPercorridos > 0 ? $kmsPercorridos.' <span class="text-xs">KMs</span>' : NULL).'</td>';
+							// Driving Session State
+							echo '<tr'.($session['ativa'] ? ($foiHoje ? ' class="table-success font-weight-bold"' : ' class="table-success"') : ($foiHoje ? ' class="font-weight-bold"' : NULL)).'>';
+							// Observations and Session Type
+							echo '<td nowrap><i style="color: Tomato;" class="fa'.($session['obs'] ? 's' : 'l').' fa-exclamation-circle" title="Observações:" data-toggle="popover" data-placement="top" data-content="'.($session['obs'] ? $session['obs'] : "Sem observações...").'"></i> <i class="'.ServicoIcon($session['tipo']).'"></i></td>';
+							// Date
+							echo '<td class="text-left" nowrap>'.date('d-m', strtotime($session['data_inicial'])).'</td>';
+							// Type of Vehicle
+							echo '<td class="text-center" nowrap>'.'<i class="'.Viatura::Icon($session["viatura_tipo"]).'"></i> '.Viatura::FormatarMatricula($session["viatura_matricula"]).'</td>';
+							// Start Location
+							echo '<td class="text-center" nowrap><a href="' . ($location["start"] ? FormatLocationURL($location["start"]) : '#') . '" target="_blank">'.date('H:i', strtotime($session['data_inicial'])).'<br/><small class="text-xs">(' . GEO::ObterEnderecoPorCoords($location["start"]) . ')</small></a></td>';
+							// Finish Location
+							echo '<td class="text-center" nowrap><a href="' . ($location["finish"] ? FormatLocationURL($location["finish"]) : '#') . '" target="_blank">'.date('H:i', strtotime($session['data_final'])).'<br/><small class="text-xs">(' . GEO::ObterEnderecoPorCoords($location["finish"]) . ')</small></a></td>';
+							// Distance Traveled
+							echo '<td class="text-right" title="Quilometros" data-toggle="popover" data-placement="top" data-content="Iniciais: '.$session['kms_iniciais'].' Finais: '.($kmsPercorridos > 0 ? $session['kms_finais'] : "Desconhecido").'">'.($kmsPercorridos > 0 ? $kmsPercorridos.' <span class="text-xs">KMs</span>' : NULL).'</td>';
 							echo '</tr>';
 						}
 					}
